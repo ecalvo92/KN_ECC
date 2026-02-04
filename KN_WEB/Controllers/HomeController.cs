a635cbc739a4a7bfbce694d75017a78b3645cb41
Eduardo Calvo Castillo <ecalvo@crnova.com>
2026-02-03 20:42:07 -0600

    Diseño del formulario de registro y ajustes visuales

--- KN_WEB/Controllers/HomeController.cs
+++ KN_WEB/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KN_WEB.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,14 +9,43 @@ namespace KN_WEB.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
 
+        #region Iniciar Sesión
+
+        [HttpGet]
         public ActionResult Login()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Login(UsuarioModel modelo)
+        {
+            return View();
+        }
+
+        #endregion
+
+        #region Registrar Usuario
+
+        [HttpGet]
+        public ActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registro(UsuarioModel model)
+        {
+            return View();
+        }
+
+        #endregion
+
     }
 }
---
+++ KN_WEB/Models/UsuarioModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KN_WEB.Models
+{
+    public class UsuarioModel
+    {
+        public string Identificacion { get; set; }
+        public string Contrasenna { get; set; }
+        public string Nombre { get; set; }
+    }
+}
