89e53b45f4b48e78a4e43d9ac5358b7278fb9032
Eduardo Calvo Castillo <ecalvo@crnova.com>
2026-02-17 18:33:24 -0600

    Fin de validaciones

---
+++ KN_WEB/Template/js/funciones/login.js
@@ -0,0 +1,28 @@
+﻿$(function () {
+  $("#FormLogin").validate({
+    rules: {
+      Identificacion: {
+        required: true
+      },
+      Contrasenna: {
+        required: true
+      }
+    },
+    messages: {
+      Identificacion: {
+        required: "Campo requerido"
+      },
+      Contrasenna: {
+        required: "Campo requerido"
+      }
+    },
+    errorElement: "span",
+    errorClass: "text-danger",
+    highlight: function (element) {
+      $(element).addClass("is-invalid");
+    },
+    unhighlight: function (element) {
+      $(element).removeClass("is-invalid");
+    }
+  });
+});
---
+++ KN_WEB/Template/js/funciones/registro.js
@@ -0,0 +1,34 @@
+﻿$(function () {
+  $("#FormRegistro").validate({
+    rules: {
+      Identificacion: {
+        required: true
+      },
+      Nombre: {
+        required: true
+      },
+      Contrasenna: {
+        required: true
+      }
+    },
+    messages: {
+      Identificacion: {
+        required: "Campo requerido"
+      },
+      Nombre: {
+        required: "Campo requerido"
+      },
+      Contrasenna: {
+        required: "Campo requerido"
+      }
+    },
+    errorElement: "span",
+    errorClass: "text-danger",
+    highlight: function (element) {
+      $(element).addClass("is-invalid");
+    },
+    unhighlight: function (element) {
+      $(element).removeClass("is-invalid");
+    }
+  });
+});
